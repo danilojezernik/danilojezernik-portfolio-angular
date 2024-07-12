4299c1f001fce1d33e315d685df21e6cdd6e8e22
danilojezernik <dani.jezernik@gmail.com>
2024-07-12 13:07:08 +0200

    add(): spinner, style for spinner, mixins, scss variables,...

--- src/app/core/pages/public/github/github.component.ts
+++ src/app/core/pages/public/github/github.component.ts
@@ -1,37 +1,45 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GithubService } from "../../../../services/api/github.service";
-import { map, Observable } from "rxjs";
+import { catchError, map, Observable, of } from "rxjs";
 import { Repo } from "../../../../models/github.model";
+import { TranslateModule, TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-github',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [ CommonModule, TranslateModule ],
   templateUrl: './github.component.html'
 })
 export class GithubComponent {
 
   _githubService = inject(GithubService)
+  _translateService = inject(TranslateService)
+  error: string | null = null;
 
-  privateLength: number = 0
+  github$: Observable<{ repos: Repo[]; length: number }> = this._githubService.getGitHubRepo().pipe(
+    map(repos => ({
+        repos: repos.map(repo => ({
+          id: repo.id,
+          name: repo.name,
+          full_name: repo.full_name,
+          owner: repo.owner,
+          html_url: repo.html_url,
+          description: repo.description,
+          language: repo.language
+        })),
+        length: repos.length
+      })
+    ),
+    catchError((error) => {
 
-  constructor() {
-    this._githubService.getGitHubRepo().subscribe((data) => {
-      this.privateLength = data.length
+      const message = error.message
+
+      this._translateService.get(message).subscribe((translation) => {
+        this.error = translation
+      })
+      return of({ repos: [], length: 0 })
     })
-  }
-
-  github$: Observable<Repo[]> = this._githubService.getGitHubRepo().pipe(
-    map(repos => repos.map(repo => ({
-      id: repo.id,
-      name: repo.name,
-      full_name: repo.full_name,
-      owner: repo.owner,
-      html_url: repo.html_url,
-      description: repo.description,
-      language: repo.language
-    })))
   )
-  
+
 }
