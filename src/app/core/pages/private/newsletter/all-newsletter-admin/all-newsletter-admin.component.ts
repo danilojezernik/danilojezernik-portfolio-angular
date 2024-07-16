1562b5279b7c666b053e671f73cf6a3619ff2553
danilojezernik <dani.jezernik@gmail.com>
2024-07-16 13:57:10 +0200

    add(): all newsletter comment

--- src/app/core/pages/private/newsletter/all-newsletter-admin/all-newsletter-admin.component.ts
+++ src/app/core/pages/private/newsletter/all-newsletter-admin/all-newsletter-admin.component.ts
@@ -9,6 +9,12 @@ import { GoBackComponent } from "../../../../../shared/components/go-back/go-bac
 import { ShowDataComponent } from "../../../../../shared/components/show-data/show-data.component";
 import { openDialogUtil } from "../../../../../utils/open-dialog.util";
 
+/**
+ * @Component AllNewsletterAdminComponent
+ * Component for managing newsletters in the administration panel.
+ * Displays a list of newsletters and allows viewing and deleting individual newsletters.
+ */
+
 @Component({
   selector: 'app-all-newsletter-admin',
   standalone: true,
@@ -16,42 +22,66 @@ import { openDialogUtil } from "../../../../../utils/open-dialog.util";
   templateUrl: './all-newsletter-admin.component.html'
 })
 export class AllNewsletterAdminComponent {
+// Injected instances: NewsletterService for newsletter data and MatDialog for dialogs
+  private _newsletterService = inject(NewsletterService) // Injected NewsletterService instance
+  private _dialog = inject(MatDialog) // Injected MatDialog instance for dialogs
 
-  private _newsletterService = inject(NewsletterService);
-  private _dialog = inject(MatDialog);
+  // BehaviorSubject to store list of newsletters
+  private _newsletterSubject = new BehaviorSubject<Newsletter[]>([])
 
-  private _newsletterSubject = new BehaviorSubject<Newsletter[]>([]);
+  // Observables to expose the newsletters list
+  newsletters$ = this._newsletterSubject.asObservable() // Observable to store list of newsletters
+  newsletterById$!: Observable<Newsletter> // Observable to store a single newsletter item
 
-
-  newsletters$ = this._newsletterSubject.asObservable();
-  newsletterById$!: Observable<Newsletter>;
-
+  /**
+   * Constructor to initialize the component.
+   * Calls the getAllNewsletter method to load all newsletters.
+   */
   constructor() {
-    this.getAllNewsletter();
+    this.getAllNewsletter() // Fetch all newsletters on component initialization
   }
 
+  /**
+   * @method getAllNewsletter
+   * Fetches all newsletters from the NewsletterService and assigns them to newsletters$.
+   */
   getAllNewsletter() {
     this._newsletterService.getAllNewsletters().subscribe(newsletter => {
-      this._newsletterSubject.next(newsletter);
-    });
+      this._newsletterSubject.next(newsletter) // Update the BehaviorSubject with fetched newsletters
+    })
   }
 
+  /**
+   * @method getNewsletterById
+   * Fetches a single newsletter by its ID from the NewsletterService and assigns it to newsletterById$.
+   * @param id - ID of the newsletter to fetch
+   */
   getNewsletterById(id: string) {
-    this.newsletterById$ = this._newsletterService.getNewsletterById(id);
+    this.newsletterById$ = this._newsletterService.getNewsletterById(id) // Fetch newsletter by ID and store in observable
   }
 
+  /**
+   * @method deleteNewsletter
+   * Deletes a newsletter by its ID.
+   * After deletion, refreshes the list of newsletters by calling getAllNewsletter().
+   * @param id - ID of the newsletter to delete
+   */
   deleteNewsletter(id?: string) {
     if (id) {
       this._newsletterService.deleteNewsletterById(id).subscribe(() => {
-        this.getAllNewsletter();
-      });
+        this.getAllNewsletter() // Refresh the list of newsletters after deletion
+      })
     }
   }
 
+  /**
+   * Method to open the dialog with newsletter details
+   * @param id - ID of the newsletter to fetch and display in the dialog
+   */
   openDialog(id?: string) {
     if (id) {
-      this.newsletterById$ = this._newsletterService.getNewsletterById(id);
-      openDialogUtil(this._dialog, id, this.getNewsletterById.bind(this), this.newsletterById$, 'title', 'newsletter');
+      this.newsletterById$ = this._newsletterService.getNewsletterById(id) // Fetch newsletter details by ID
+      openDialogUtil(this._dialog, id, this.getNewsletterById.bind(this), this.newsletterById$, 'title', 'newsletter') // Open dialog with fetched data
     }
   }
 }
