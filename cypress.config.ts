1c1c3562d8fbcd2ee37c23fcc4e70e88fb02816a
Danilo Jezernik <dani.jezernik@gmail.com>
2024-09-22 15:48:49 +0200

    add(): swiper to the project in README.md

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     chromeWebSecurity: false, // Try disabling web security if it's blocking tests
   },
-
+  projectId: "h8tgs1",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
